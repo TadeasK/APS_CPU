db8999deac2ec52073ea867810a882a961b5c878
Tadeáš Kouba <koubatad@fit.cvut.cz>
2022-11-08 19:03:20 +0100

    S1: Finnished processor coding and debugging

--- semestralka1/kouba_tadeas_CPU.v
+++ semestralka1/kouba_tadeas_CPU.v
@@ -10,82 +10,88 @@ module processor( input         clk, reset, //
                   output        WE, //
                   output [31:0] address_to_mem, //
                   output [31:0] data_to_mem, //
-                  input  [31:0] data_from_mem
+                  input  [31:0] data_from_mem //
                 );
     
-    reg ALUSrc; //
-    wire [2:0] AluControl; //
-    reg MemWrite; //
-    reg MemToReg; //
-    reg RegWrite; //
-    reg BranchBeq; //
-    reg BranchJal; //
-    reg BranchJalr; //
-    wire [2:0] immControl; //
+    wire ALUSrc; // 0/1 Which source to use for SrcB
+    wire [2:0] AluControl; // 0-7 which operation should ALU make
+    wire MemWrite; // 0/1 Write to memory?
+    wire MemToReg; // 0/1 Which source to use for registerSet write
+    wire RegWrite; // 0/1 Write to register?
+    wire BranchBeq; // 0/1 Branch?
+    wire BranchJal; // 0/1 Jump? 
+    wire BranchJalr; // 0/1 Return?
+    wire [2:0] immControl; // 0-7 Controls how immDecode decodes imm
+
+    wire [31:0] ImmOp; // return value of immDecode
+    wire [31:0] res; // Value to write to register
+    wire [31:0] Reg1; // Value from register1
+    wire [31:0] Reg2; // Value from register2
+   
+    wire [31:0] SrcB; // Operand2 of ALU (Decided by ALUSrc - can be ImmOp or Reg2)
+
+    wire [31:0] ALUout; // Output of ALU
+    wire Zero; // 1 if ALUout == 0 else 1
+    wire [31:0] branchTarget;
+    reg BranchJalx;
+    reg BranchOutcome;
+    wire [31:0] brJalxMuxOut;
+    wire [31:0] PCn;
     
-    ctrl CTRL_unit(instruction[6:0], instruction[31:25], 
-                   instruction[12:14],
-                   ALUSrc, MemWrite, MemToReg, RegWrite, BranchBeq,
+    wire [31:0] pc = 32'b0;
+    wire [31:0] PCPlus4;
+    wire [31:0] ImmOpPlusPC;
+    wire [31:0] SrcA;
+    wire [31:0] readData;    
+
+    assign PCPlus4 = pc + 4;
+    assign ImmOpPlusPC = ImmOp + pc;
+    assign SrcA = Reg1; // Operand1 of ALU 
+    assign readData = data_from_mem; // Data read from data memory 
+
+    always @(BranchBeq or BranchJal or BranchJalr)
+    begin
+        BranchJalx = BranchJal || BranchJalr;
+        BranchOutcome = (BranchBeq && Zero) || BranchJalx;
+    end
+
+
+    CTRL_unit ctrl(instruction[6:0], instruction[31:25], 
+                   instruction[14:12],
+                   ALUSrc, AluControl, MemWrite, MemToReg, RegWrite, BranchBeq,
                    BranchJal, BranchJalr, immControl
                    );
     
-    wire [31:0] ImmOp;
-    wire [31:0] res;
-    wire [31:0] Reg1;
-    wire [31:0] Reg2;
-    
-    immCtrl immControl(instruction[31:7],
+    immDecode immDec(instruction[31:7],
                        immControl,
                        ImmOp);
 
-    registers registerSet(instruction[19:15], 
+    registerSet registers(instruction[19:15], 
                           instruction[24:20],
                           instruction[11:7],
                           clk, RegWrite,
                           res, Reg1, Reg2);
     
-    wire [31:0] SrcA = Reg1;
-    wire [31:0] SrcB;
-    reg Zero;
-    wire [31:0] ALUout;
-    wire [31:0] readData;
-    wire [31:0] branchTarget;
-    wire [31:0] PC;
-
-    aluMux mux2_1(Reg2, ImmOp, ALUSrc, SrcB);
-
-    alu ALU(AluControl, SrcA, SrcB, ALUout, Zero);
+    ALU alu(AluControl, SrcA, SrcB, ALUout, Zero);
     
-    dataMem data_mem(clk, MemWrite, ALUout, SrcB, readData);
-
-
-    branchTargetMux mux2_1( /*kombinace?TODO*/,ALUout,BranchJalr,branchTarget);
-
-    reg BranchJalx;
-    reg BranchOutcome;
-
-    always @(BranchBeq or BranchJal or BranchJalr)
-    begin
-        BranchJalx <= BranchJal || BranchJalr;
-        BranchOutcome <= (BranchBeq && Zero) || BranchJalx;
-    end
-
-    wire [31:0] branchJalxMuxOut;
-    wire [31:0] PCn;
-    wire [31:0] pc;
-
-    branchJalxMux mux2_1(ALUout, /*PCplus4TODO*/, BranchJalx, branchJalxMuxOut );
+    register PCRegister(PCn, clk, reset, pc);
     
-    dataMemMux mux2_1( branchJalxMuxOut, readData, MemToReg, res);
+    mux2_1 aluMux(Reg2, ImmOp, ALUSrc, SrcB);
+    
+    mux2_1 brTargetMux( ImmOpPlusPC,ALUout,BranchJalr,branchTarget);
 
-    PCMux mux2_1(/*PCplus4*/, res, BranchOutcome, PCn)
+    mux2_1 brJalxMux(ALUout, PCPlus4, BranchJalx, brJalxMuxOut );
+    
+    mux2_1 dataMemMux( brJalxMuxOut, readData, MemToReg, res);
 
-    PCRegister register(PCn, clk, pc);
+    mux2_1 PCMux(PCPlus4, branchTarget, BranchOutcome, PCn);
+
 
     assign PC = pc;
     assign WE = MemWrite;
     assign address_to_mem = ALUout;
-    assign data_to_mem = SrcB;
+    assign data_to_mem = Reg2;
+
 endmodule
 //=========================================================================================
 //=========================================================================================
@@ -94,8 +100,8 @@ endmodule
 //=========================================================================================
 module ALU( input [2:0] ALUControl, 
             input [31:0] SrcA, SrcB,
-            output [31:0] ALUout, 
-            output Zero
+            output reg [31:0] ALUout, 
+            output reg Zero
           );
 
 // ALUControl
@@ -106,7 +112,7 @@ module ALU( input [2:0] ALUControl,
 // 100 = /
 // 101 = %
 // 110 = !<
-// 111 = 
+// 111 = LUI
 
     always @(*)
     begin
@@ -117,9 +123,9 @@ module ALU( input [2:0] ALUControl,
             3: ALUout = SrcA < SrcB;
             4: ALUout = SrcA / SrcB;
             5: ALUout = SrcA % SrcB;
-            6: ALUout = SrcA + SrcB;
-            7: ALUout = !(SrcA < SrcB);
-            default: ALUout = X;
+            6: ALUout = !(SrcA < SrcB);
+            7: ALUout = {SrcB, 12'b0};
+            default: ALUout = 32'bX;
         endcase
     end
 
@@ -136,9 +142,9 @@ endmodule
 //                 IMMIDIATE CONTROL
 //=========================================================================================
 //=========================================================================================
-module immControl(input inst[24:0], // value of instruction[31:7]
+module immDecode(input [24:0] inst, // value of instruction[31:7]
                   input [2:0] control,
-                  output [31:0] immOp
+                  output reg [31:0] immOp
                  );
 // immControl
 // 000 I-Type
@@ -146,6 +152,7 @@ module immControl(input inst[24:0], // value of instruction[31:7]
 // 010 B-Type
 // 011 J-Type
 // 100 U-Type
+
 
     always @(*)
     begin
@@ -187,11 +194,11 @@ module CTRL_unit( input [6:0] opCode, funct7,
 // 100 = /    // 011 J-Type
 // 101 = %    // 100 U-Type
 // 110 = !<
-// 111 = ERROR
+// 111 = LUI
 
     always @(*)
     begin
-        casez ({opCode,funct7,funct3)
+        casez ({opCode,funct7,funct3})
             'b0110011_0000000_000: out = 'b0_000_0_0_1_0_0_0_XXX;   // add
 
             'b0010011_???????_000: out = 'b1_000_0_0_1_0_0_0_000;   // addi
@@ -205,9 +212,9 @@ module CTRL_unit( input [6:0] opCode, funct7,
             'b1100011_???????_000: out = 'b0_010_0_X_0_1_0_0_010;   // beq
             'b1100011_???????_100: out = 'b0_110_0_X_0_1_0_0_010;   // blt
 
-            'b0000011_???????_010: out = 'bX_000_0_X_1_0_0_0_000;   // lw
+            'b0000011_???????_010: out = 'b1_000_0_1_1_0_0_0_000;   // lw
             'b0100011_???????_010: out = 'bX_000_1_X_0_0_0_0_001;   // sw
-            'b0110111_???????_???: out = 'b0_000_0_1_1_0_0_0_100;   // lui TODO
+            'b0110111_???????_???: out = 'b1_111_0_0_1_0_0_0_100;   // lui
 
             'b1101111_???????_???: out = 'bX_XXX_0_0_1_0_1_0_011;   // jal
             'b1100111_???????_000: out = 'b1_000_0_0_1_0_0_1_000;   // jalr
@@ -216,15 +223,15 @@ module CTRL_unit( input [6:0] opCode, funct7,
         endcase
     end
 
-    assign ALUSrc = out[11];
-    assign ALUControl[2:0] = out[10:8];
-    assign MemWrite = out[7];
-    assign MemToReg = out[6];
-    assign RegWrite = out[5];
-    assign BranchBeq = out[4];
-    assign BranchJal = out[3];
-    assign BranchJalr = out[2];
-    assign [1:0] immControl = out[1:0];
+    assign ALUSrc = out[12];
+    assign ALUControl = out[11:9];
+    assign MemWrite = out[8];
+    assign MemToReg = out[7];
+    assign RegWrite = out[6];
+    assign BranchBeq = out[5];
+    assign BranchJal = out[4];
+    assign BranchJalr = out[3];
+    assign immControl = out[2:0];
 
 endmodule
 //=========================================================================================
@@ -232,12 +239,16 @@ endmodule
 //                 Multiplexer 2 to 1
 //=========================================================================================
 //=========================================================================================
-module mux2_1(	input [31:0] a,b, select,
-		output reg y);
+module mux2_1(	input [31:0] a,b, 
+                input select,
+		output reg [31:0] y);
 
 	always@(*)
+    begin
 		if (select==0) y = a;
 		else y = b;
+    end
+
 endmodule
 //=========================================================================================
 //=========================================================================================
@@ -245,22 +256,27 @@ endmodule
 //=========================================================================================
 //=========================================================================================
 module registerSet ( input [4:0] A1, A2, A3,
-                     input clk, WE3
+                     input clk, WE3,
                      input [31:0] WD3,
                      output reg [31:0] RD1, RD2
                     );
 
     reg [31:0] rf[31:0];
-    rf[0] = 0;
+    wire zero = 0;
+
+    always @(*)
+    begin
+        rf[0] <= zero;
+    end
 
     always @(A1)
     begin
-        RD1 = rf[A1]
+        RD1 = rf[A1];
     end
-
+    
     always @(A2)
     begin
-        RD2 = rf[A2]
+        RD2 = rf[A2];
     end
 
     always @(posedge clk)
@@ -272,13 +288,21 @@ module registerSet ( input [4:0] A1, A2, A3,
         else;
     end
 endmodule
-
+//=========================================================================================
+//=========================================================================================
+//                 PC REGISTER
+//=========================================================================================
+//=========================================================================================
 module register(input [31:0] in,
-            input clk,
+            input clk, reset,
             output reg [31:0] out);
-    always @(posedge clk)
+    
+    always @(posedge clk or reset)
     begin
-        out = in;
+        if (reset==1)
+            out = 0;
+        else
+            out = in;
     end
 endmodule
 //=========================================================================================
